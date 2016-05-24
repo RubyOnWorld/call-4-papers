63d8b2170bdcec7bba34a597c4b44a84db40c01d
Florian Gilcher <florian.gilcher@asquera.de>
2016-05-24 10:43:54 +0000

    Switch to plain input

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325234754) do
+ActiveRecord::Schema.define(version: 20160524103034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,7 +156,7 @@ ActiveRecord::Schema.define(version: 20150325234754) do
     t.string   "last_sign_in_ip"
     t.boolean  "staff"
     t.boolean  "mentor",                 default: false
-    t.integer  "gender"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
